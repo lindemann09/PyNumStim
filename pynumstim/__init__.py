e3c82415ed495ab701543807dc4465dc03a7a128
Oliver Lindemann <lindemann09@googlemail.com>
2024-04-23 16:06:28 +0200

    bump version

--- pynumstim/__init__.py
+++ pynumstim/__init__.py
@@ -4,7 +4,7 @@ creating math problem stimuli with latex formulas and dvipng
 """
 
 __author__ = "Oliver Lindemann"
-__version__ = "0.2.10"
+__version__ = "0.2.11-dev"
 
 from ._problem import MathProblem
 from ._mplist import MathProblemList
