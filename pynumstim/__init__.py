60417006475cb32e36868bcb9c291b658a94ebb0
Oliver Lindemann <lindemann09@googlemail.com>
2024-12-04 11:17:19 +0100

    two setp math problem

--- pynumstim/__init__.py
+++ pynumstim/__init__.py
@@ -4,9 +4,10 @@ creating math problem stimuli with latex formulas and dvipng
 """
 
 __author__ = "Oliver Lindemann"
-__version__ = "0.2.11-dev"
+__version__ = "0.2.11-dev2"
 
-from ._problem import SimpleMathProblem, SimpleMathProblem
+from ._problem import SimpleMathProblem, MathProblem
+from ._two_step_problem import TwoStepMathProblem
 from ._mplist import MathProblemList
 from ._data_sets import Datasets
 from ._number import Num, TNum, TPyNum
---
+++ pynumstim/_two_step_problem.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+from typing import Any, Dict, Optional, Set
+from ._problem import MathProblem, SimpleMathProblem, TProperties
+from ._number import Num, TNum, TPyNum
+
+
+class TwoStepMathProblem(MathProblem):
+
+    def __init__(
+        self,
+        operand1: TNum,
+        operand2: TNum,
+        operand3: TNum,
+        operation1: str,
+        operation2: str,
+        result: Optional[TNum] = None,
+        properties: Optional[TProperties] = None,
+    ) -> None:
+        """properties: dict of properties.
+        Keys represent the property name and must be text strings
+        """
+
+        self.step1 = SimpleMathProblem(
+            operand1=operand1,
+            operation=operation1,
+            operand2=operand2)
+        self.step2 = SimpleMathProblem(
+            operand1=self.step1.calc(),
+            operation=operation2,
+            operand2=operand3,
+            result=result,
+            properties=properties
+        )
+
